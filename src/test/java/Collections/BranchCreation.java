2f9a115b1ffa9075a2e90a1d664c6c65ef1d2f83
Dell <Dell@DESKTOP-NNILPMS>
2023-06-12 15:12:38 +0530

    printing the message

--- src/test/java/Collections/BranchCreation.java
+++ src/test/java/Collections/BranchCreation.java
@@ -14,7 +14,10 @@ public class BranchCreation {
 
 	@Test
 	public static void branch_creation() {
-		
-		given().contentType(ContentType.JSON).headers("X-Auth-Token",Authority_token.X_auth()).body(new File("./Collections/branchcreation.json")).when().post(Authority_token.URL+"/api/branches").then().log().body();
+
+		given().contentType(ContentType.JSON).headers("X-Auth-Token", Authority_token.X_auth())
+				.body(new File("./Collections/branchcreation.json")).when().post(Authority_token.URL + "/api/branches")
+				.then().log().body();
+		System.out.println("new changes");
 	}
 }
